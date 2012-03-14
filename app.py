2002679f58157f88ec52c383146230ea8ab46e54
Yuval Adam <yuv.adm@gmail.com>
2012-03-14 14:45:14 -0700

    short signature ids

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CLOUDANT_BASE = 'https://pinkyswear.cloudant.com/pinkyswear/'
 CLOUDANT_AUTH = ('pinkyswear', 'abc123')
 
 @route('/')
-def index(name='home'):
+def index():
     return template('index.html', sig=None)
 
 @route('/js/:filename')
@@ -26,11 +26,12 @@ def sig(id='home'):
     return template('index.html', sig=sig)
 
 @post('/s')
-def sig(id='home'):
+def sig():
+    id = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
     data = json.dumps({ 's' : request.POST['output'] })
-    url = CLOUDANT_BASE
+    url = CLOUDANT_BASE + id
     headers = { 'content-type' : 'application/json' }
-    r = requests.post(url, auth=CLOUDANT_AUTH, data=data, headers=headers)
+    r = requests.put(url, auth=CLOUDANT_AUTH, data=data, headers=headers)
     redirect('/s/%s' % json.loads(r.content)['id'])
 
 run(host='localhost', port=8000)
