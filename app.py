89d5cd575fb2665a8d4934b635457e79655df893
Yuval Adam <yuv.adm@gmail.com>
2012-03-13 23:51:23 -0700

    added new sig post method

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from bottle import route, run
 
 @route('/')
@@ -7,5 +10,10 @@ def index(name='home'):
 @route('/s/:id')
 def sig(id='home'):
     return '<p><b>Signature</b></p><p>%s</p>' % id
+
+@post('/s')
+def sig(id='home'):
+    id = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
+    return id
 
 run(host='localhost', port=8000)
