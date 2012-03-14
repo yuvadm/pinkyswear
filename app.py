877986c4eab288a28a183df5e73c89462ec7baf2
Yuval Adam <yuv.adm@gmail.com>
2012-03-13 23:40:37 -0700

    basic routing

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from bottle import route, run
 
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
+@route('/')
+def index(name='home'):
+    return '<p><b>Homepage</b></p><p>Insert widget here</p>'
+
+@route('/s/:id')
+def sig(id='home'):
+    return '<p><b>Signature</b></p><p>%s</p>' % id
 
 run(host='localhost', port=8000)
